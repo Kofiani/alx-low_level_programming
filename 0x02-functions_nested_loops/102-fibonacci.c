3a22d9c09a95ba1876f7a68f82c8a195e6dd542a
Kofiani <samuelannie.k@gmail.com>
2022-06-07 21:21:43 +0000

    testing

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int a;
 	long int n1, n2, fn;
-	
+
 	n1 = 1;
 	n2 = 2;
 	printf("%ld, %ld", n1, n2);
@@ -16,8 +16,10 @@ int main(void)
 	{
 		fn = n1 + n2;
 		printf(", %ld", fn);
-		n1 = n2;   n2 = fn;
+		n1 = n2;
+		n2 = fn;
 	}
 	printf("\n");
+	
 	return (0);
 }
