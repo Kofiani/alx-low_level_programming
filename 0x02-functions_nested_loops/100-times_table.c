396064971adbb725b0c59d35812da813586ec96d
Kofiani <samuelannie.k@gmail.com>
2022-06-07 21:03:14 +0000

    testing

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h" 
+/**
+ * print_times_table - function
+ * @n: parameter
+ *
+ * Return: 0.  
+ */
+void print_times_table(int n)
+{
+	int m,p, k;   
+	if (n >= 0 && n <= 15)
+	{
+		for (m = 0; m <= n; a++)
+		{
+			_putchar('0');
+			for (p = 1; p <= n; p++)
+			{
+				k = m * p;
+				_putchar(',');
+				_putchar(' ');
+					if (k <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(k + '0');
+					}
+					else if (k <= 99)
+					{
+						_putchar(' ');
+						_putchar((k / 10) + '0');
+						_putchar((k % 10) + '0');
+					}
+					else
+					{
+						_putchar(((k / 100) % 10) + '0');
+						_putchar(((k / 10) % 10) + '0');
+						_putchar((k % 10) + '0');
+					}
+			}
+		       _putchar('\n');
+		}
+	}
+}
