1fc8c285aa00ff1b34ac17fa551eb73eb3a88012
Kofiani <samuelannie.k@gmail.com>
2022-06-07 23:29:11 +0000

    Fixed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,23 +9,20 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar('0');
+		for (j = 1; j < 10; j++)
 		{
 			m = (i * j);
+			_putchar(',');
+			_putchar(' ');
 			if (m > 9)
 			{
-				_putchar(' ');
 				_putchar(m / 10 + '0');
 				_putchar(m % 10 + '0');
 			} else if (m < 10)
 			{
 				_putchar(' ');
-				_putchar(' ');
 				_putchar(m % 10 + '0');
-			}
-			if (j < 9)
-			{
-			_putchar(',');
 			}
 		}
 		_putchar('\n');
