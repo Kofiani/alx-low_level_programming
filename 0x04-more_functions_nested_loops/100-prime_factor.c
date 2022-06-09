05a007d6c4277faef3f7723381a605fc83284d7d
Kofiani <samuelannie.k@gmail.com>
2022-06-09 19:27:58 +0000

    Test

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Function
+ * Return: 0.
+ */
+
+int main(void)
+{
+	long int c, a;
+	
+	c = 612852475143;
+	for (a = 2; a <= n; a++)
+	{
+		if (c % a == 0)
+		{
+			c /= a;
+			a--;
+		}
+	}
+	printf("%ld\n", a);
+	return (0);
+}
