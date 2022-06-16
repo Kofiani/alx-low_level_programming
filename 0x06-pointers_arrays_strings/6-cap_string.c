cf6c618dc772d02dbb9fc0eae7159a2f3b745443
Kofiani <samuelannie.k@gmail.com>
2022-06-16 07:56:00 +0000

    First test

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,10 +12,10 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 32 && str[i] <= 47 || str[i] >= 58 && str[i] <= 64
-				|| str[i] >= 91 && str[i] <= 96
-				|| str[i] >= 123 && str[i] <= 126 || str[i] == 9 || str[i] == 2
-				|| str[i] >= 48 && str[i] <= 57 || str[i] == 10)
+		if ((str[i] >= 32 && str[i] <= 47) || (str[i] >= 58 && str[i] <= 64)
+				|| (str[i] >= 91 && str[i] <= 96)
+				|| (str[i] >= 123 && str[i] <= 126) || (str[i] == 9 || str[i] == 2)
+				|| (str[i] >= 48 && str[i] <= 57) || (str[i] == 10))
 		{
 			if (str[i + 1] >= 97 && str[i + 1] <= 122)
 			{
