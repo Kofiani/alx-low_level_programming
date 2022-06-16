e18c11ee93a8c429366f2214f9d9ffc808b416d4
Kofiani <samuelannie.k@gmail.com>
2022-06-16 13:07:30 +0000

    fixed

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - function
+ * @rot: parameter
+ * Return: char
+ */
+
+char *rot13(char *rot)
+{
+	char alph[] ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] ="nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int c = 0, i;
+
+	while (rot[c] = '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (rot[c] == alph[i])
+			{
+				rot[c] = rot13[i];
+				break;
+			}
+		}
+		c++;
+	}
+	return (rot);
+}
--- 0x06-pointers_arrays_strings/main_test.c
+++ 0x06-pointers_arrays_strings/main_test.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include "main.h"
 
-int main(void) 
-{
-	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+int main(void) {
+   	char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
    	char *p;
-  	p = leet(s);
+  	p = rot13(s);
    	printf("%s", p);
+   	printf("------------------------------------\n");
+   	printf("%s", s);     printf("------------------------------------\n");
+   	p = rot13(s);
+   	printf("%s", p);
+   	printf("------------------------------------\n");
    	printf("%s", s);
-   	return (0); 
+   	printf("------------------------------------\n");
+   	p = rot13(s);
+   	printf("%s", p);
+   	printf("------------------------------------\n");
+   	printf("%s", s);
+   	return (0);
 }
