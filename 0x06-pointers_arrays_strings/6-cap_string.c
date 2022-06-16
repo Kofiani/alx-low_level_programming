1b1bfa0512dadd3981c61f97f4d0d53e876ae370
Kofiani <samuelannie.k@gmail.com>
2022-06-16 10:22:45 +0000

    First test

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,9 +10,9 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	while(str[i])
+	while (str[i])
 	{
-		while(!(str[i] >= 97 && str[i] <= 122))
+		while (!(str[i] >= 97 && str[i] <= 122))
 			i++;
 		if (str[i - 1] == 32 ||
 		    str[i - 1] ==  9 ||
@@ -28,7 +28,9 @@ char *cap_string(char *str)
 		    str[i - 1] == 123 ||
 		    str[i - 1] == 125 ||
 		    i == 0)
-				str[i] -= 32;
+		{
+			str[i] -= 32;
+		}
 		i++;
 
 	}
