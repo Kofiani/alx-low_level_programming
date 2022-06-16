7b89bb7ecb0d60713463683a2e48dd24060c0b90
Kofiani <samuelannie.k@gmail.com>
2022-06-16 12:18:38 +0000

    fixed

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,24 +6,26 @@
  * Return: pointer
  */
 
-char *leet(char *a)
+char *leet(char *str)
 {
-	char b[] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	char low_c[] = {97, 101, 111, 116, 108};
+	char upp_c[] = {65, 69, 79, 84, 76};
+	int num[] = {52, 51, 48, 55, 49};
 	int c = 0, i;
 
-	while (a[c])
+	while (str[c] !='\0')
 	{
-		for (i = 0; i < 7; i++)
+		for (i = 0; i < 5; i++)
 		{
-			if (a[c] == b[i] ||
-			    a[c] - 32 == b[i])
+			if (str[c] == low_c[i] ||
+			    str[c] == upp_c[i])
 			{
-				a[c] = i + 0;
+				str[c] = num[i];
 
 
 			}
 		}
 		c++;
 	}
-	return (a);
+	return (str);
 }
