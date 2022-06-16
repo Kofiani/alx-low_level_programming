a9d367afa73d9287243a681ac3fe6944f177d2c8
Kofiani <samuelannie.k@gmail.com>
2022-06-16 10:19:38 +0000

    First test

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,28 +8,31 @@
 
 char *cap_string(char *str)
 {
-	int i, j;
-	int spe[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 48, 49,
-		     50, 51, 52, 53, 54, 55, 56, 57};
+	int i = 0;
+	int j = 0;
 
-	if (str[0] >= 97 && str[0] <= 122)
+	while(str[i])
 	{
-		str[0] = str[0] - 32;
-	}
-	i = 0;
-	while (str[i] != '\0')
-	{
-		for (j = 0; j < 24; j++)
-		{
-			if (str[i] >= 97 && str[i] <= 122)
-			{
-				if (str[i - 1] == spe[j])
-				{
-					str[i] = str[i] - 32;
-				}
-			}
-		}
+		while(!(str[i] >= 97 && str[i] <= 122))
+			i++;
+		if (str[i - 1] == 32 ||
+		    str[i - 1] ==  9 ||
+		    str[i - 1] == 10 ||
+		    str[i - 1] == 44 ||
+		    str[i - 1] == 59 ||
+		    str[i - 1] == 46 ||
+		    str[i - 1] == 33 ||
+		    str[i - 1] == 63 ||
+		    str[i - 1] == 34 ||
+		    str[i - 1] == 40 ||
+		    str[i - 1] == 41 ||
+		    str[i - 1] == 123 ||
+		    str[i - 1] == 125 ||
+		    i == 0)
+				str[i] -= 32;
 		i++;
+
 	}
+
 	return (str);
 }
