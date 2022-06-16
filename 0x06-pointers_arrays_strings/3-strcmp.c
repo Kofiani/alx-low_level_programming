7c7b8f84b7b8994793dcba987101bf29123d0f4d
Kofiani <samuelannie.k@gmail.com>
2022-06-16 10:27:43 +0000

    First test

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,24 +8,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j =0;
 
-	while (s1[i] !='\0')
+	while (*(s1 && *s2) && (*s1 == *s2))
 	{
-		i++;
+		s1++;
+		s2++;
 	}
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-	if (i < j)
-	{
-		return (-15);
-	}
-	else if (i > j)
-	{
-		return (15);
-	}
-	return (0);
+	return (*s1 - *s2);
 
 }
