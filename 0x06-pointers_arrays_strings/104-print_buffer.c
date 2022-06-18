99eb0f1ec92dc58e146b4746b48189753d60799f
Kofiani <samuelannie.k@gmail.com>
2022-06-18 00:46:10 +0000

    Updated

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -20,7 +20,6 @@ void print_buffer(char *b, int size)
 		{
 			if ((index + byte) >= size)
 				printf(" ");
-
 			else
 				printf("%02x", *(b + index + byte));
 			if ((index % 2) != 0 && index != 0)
@@ -28,14 +27,17 @@ void print_buffer(char *b, int size)
 		}
 		for (index = 0; index < 10; index++)
 		{
-			if ((index + byte) >= size) break;
+			if ((index + byte) >= size)
+				break;
 			else if (*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
-					printf("%c", *(b + index + byte));
+				printf("%c", *(b + index + byte));
 			else
 				printf(".");
 		}
-		if (byte >= size) continue;
+		if (byte >= size)
+			continue;
 		printf("\n");
 	}
-	if (size <= 0) printf("\n");
+	if (size <= 0)
+		printf("\n");
 }
