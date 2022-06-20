78dbc89f4f0a887b86de6263c1c61e6d43e075c3
Kofiani <samuelannie.k@gmail.com>
2022-06-20 14:26:45 +0000

    Updated

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,16 +9,10 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
 	unsigned int j;
-
-	while (*dest != '\0')
-	{
-		i++;
-	}
 	for (j = 0; j < n; j++)
 	{
-		*(dest + i + j) = *(src + j);
+		*(dest + j) = *(src + j);
 	}
 	return (dest);
 }
