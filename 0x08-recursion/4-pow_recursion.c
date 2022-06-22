9fd015117994735eea465cff4b4340a1b49d7653
Kofiani <samuelannie.k@gmail.com>
2022-06-22 19:04:15 +0000

    Pushed pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function to find exponent of integer
+ * @x: integer to convert
+ * @y: exponent
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int i = 1;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	i *= _pow_recursion(x, y - 1);
+
+	i *= x;
+
+	return (i);
+}
