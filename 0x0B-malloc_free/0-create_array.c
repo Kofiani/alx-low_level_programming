c9ca9b63ed8cb8ed5d4bfc46104762e4895b4107
Kofiani <samuelannie.k@gmail.com>
2022-06-27 16:22:06 +0000

    Minor Update

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array with dynamic allocation
+ * @size: the size dynamically allocate pointer to
+ * @c: character to insert into character array.
+ * Return: a pointer value.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *ptr = (sizeof(c) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		if (size == 0)
+			return (NULL);
+		else if (ptr == NULL)
+			return (NULL);
+		else
+			ptr[i] = c;
+	}
+
+	return (ptr);
+}
