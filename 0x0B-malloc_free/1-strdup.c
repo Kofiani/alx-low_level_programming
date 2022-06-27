84ae42db8ab4ae768ca4d64dd1fbfb78d7b80a0e
Kofiani <samuelannie.k@gmail.com>
2022-06-27 19:00:46 +0000

    Minor Update

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,52 @@
+#include "main.h"
+int strLen(char *s);
+/**
+ * strLen - counts the number of chars in a string
+ * @s: the string pointer variable
+ * Return: integer value
+ */
+
+int strLen(char *s)
+{
+	int i = 0;
+
+	while (*s)
+	{
+		s++;
+	}
+
+	return (i);
+}
+
+/**
+ * _strdup - duplicates string into another memory
+ * @str: string pointer variable
+ * Return: pointer.
+ */
+
+char *_strdup(char *str)
+{
+	int i, len = strLen(str);
+	char *ptr = malloc(sizeof(*ptr) * len);
+
+	if (str == NULL)
+	{
+		ptr = NULL;
+	}
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		i = 0;
+		while (*str)
+		{
+			ptr[i] = *str;
+			i++;
+			str++;
+		}
+
+		return (ptr);
+	}
+}
--- 0x0B-malloc_free/main_test.c
+++ 0x0B-malloc_free/main_test.c
@@ -1,7 +1,4 @@
 #include "main.h" 
 #include <stdio.h> 
 #include <stdlib.h>  
-
-void simple_print_buffer(char *buffer, unsigned int size) 
-{     unsigned int i;      i = 0;     while (i < size)     {         if (i % 10)         {             printf(" ");         }         
-	if (!(i % 10) && i)         {             printf("\n");         }         printf("0x%02x", buffer[i]);         i++;     }     printf("\n"); }  /**  * main - check the code for ALX School students.  *  * Return: Always 0.  */ int main(void) {     char *buffer;      buffer = create_array(0, 'H');     if  (buffer == NULL)     {         printf("failed to allocate memory\n");         return (1);     }     simple_print_buffer(buffer, 98);     free(buffer);     return (0); }
+int main(void) {     char *s;      s = _strdup("ALX SE");     if (s == NULL)     {         printf("failed to allocate memory\n");         return (1);     }     printf("%s\n", s);     free(s);     return (0); }
