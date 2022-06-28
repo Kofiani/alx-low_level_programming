acc2cbe095a35c0e18dce60978891ac9f3a1053f
Kofiani <samuelannie.k@gmail.com>
2022-06-28 19:19:29 +0000

    Update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ptr = malloc(sizeof(**ptr) * height);
+	ptr = malloc(sizeof(int *) * height);
 
 	if (ptr == NULL)
 		return (NULL);
