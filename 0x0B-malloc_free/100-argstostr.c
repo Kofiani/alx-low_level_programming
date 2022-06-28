5f466d585704c12b0a7061cd846e909c58009981
Kofiani <samuelannie.k@gmail.com>
2022-06-28 19:39:06 +0000

    Update

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * argstorstr - concatenates arguments from CLI
+ * @ac - count from cli
+ * @av: pointer to variable
+ * Return: pointer variable
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int arg, byte, i, size = ac;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
+	}
+
+	str = malloc(sizeof(char) * size + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+		{
+			str[i++] = av[arg][byte]; 
+		}
+		str[i] = '\n';
+	}
+	str[size] = '\0';
+
+	return (str);
+}
