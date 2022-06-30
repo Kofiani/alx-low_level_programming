0f6621f9808d64f189a4007dbc9d98354b6f1016
Kofiani <samuelannie.k@gmail.com>
2022-06-30 14:04:52 +0000

    Minor updates

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	for (i = 0; i < nmemb; i++)
 		mem[i] = malloc(size);
 
-	for (i = 0; i < nmemb * size; i++)
+	for (i = 0; i < nmemb; i++)
 		mem[i] = '\0';
 
 	return (mem);
