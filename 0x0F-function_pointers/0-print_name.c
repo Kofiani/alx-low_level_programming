8a41a1c6f785b3c5c09058933816d38f30383a36
Kofiani <samuelannie.k@gmail.com>
2022-07-06 19:56:02 +0000

    Fixed

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function pointer to print name.
+ * @name: name variable
+ * @f: pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+
+	f(name);
+}
