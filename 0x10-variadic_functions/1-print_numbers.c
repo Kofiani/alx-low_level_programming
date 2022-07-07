48940655efe862d9530c93b87318d06d94f5eec2
Kofiani <samuelannie.k@gmail.com>
2022-07-07 14:22:07 +0000

    Task Done

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function to print numbers
+ * @separator: string pointer variable.
+ * @n: number of variables passed
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int num = 0;
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(ap, int);
+		printf("%d", num);
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s ", separator);
+		}
+	}
+	va_end(ap);
+	printf("\n");
+}
--- 0x10-variadic_functions/main_test.c
+++ 0x10-variadic_functions/main_test.c
@@ -1,3 +1,4 @@
 #include <stdio.h> 
-#include "variadic_functions.h"  
-/**  * main - check the code  *  * Return: Always 0.  */ int main(void) {     int sum;      sum = sum_them_all(2, 98, 1024);     printf("%d\n", sum);     sum = sum_them_all(4, 98, 1024, 402, -1024);     printf("%d\n", sum);     return (0); }
+#include "variadic_functions.h" 
+
+int main(void) {     print_numbers(NULL, 4, 0, 98, -1024, 402);     return (0); }
