75e132b4186baff62a3e9611711da6d5b2ede314
Kofiani <samuelannie.k@gmail.com>
2022-07-07 22:27:34 +0000

    Task Done

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,18 @@
  * Desc: Header file for variadic functions.
  */
 
+/**
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ * a data type corresponding to symbol.
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer_t
+
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
