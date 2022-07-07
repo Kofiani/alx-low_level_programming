653ca6b1610e2cd86a558fae8828b800546046ed
Kofiani <samuelannie.k@gmail.com>
2022-07-07 16:08:06 +0000

    Task Done

--- 0x10-variadic_functions/main_test.c
+++ 0x10-variadic_functions/main_test.c
@@ -1,4 +1,4 @@
 #include <stdio.h> 
 #include "variadic_functions.h" 
 
-int main(void) {     print_numbers(NULL, 4, 0, 98, -1024, 402);     return (0); }
+int main(void) {     print_strings(", ", 2, NULL, "Django");     return (0); }
