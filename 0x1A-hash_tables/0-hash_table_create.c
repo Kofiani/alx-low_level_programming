9cad938b00f08e84186f928b83fb892f34d3a963
Kofiani <samuelannie.k@gmail.com>
2022-09-24 18:10:37 +0100

    Created hash table and the hash table header file

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "hash_table.h"
+
+/**
+* hash_table_create - creates a table
+* @size: sets the size for the table
+* Return: returns a pointer to the table
+**/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < table->size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+# Thi is the directory for the project Hash Tables
