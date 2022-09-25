6006b3655eb8f7db4f18186eae078befebb5c78c
Kofiani <samuelannie.k@gmail.com>
2022-09-25 16:33:41 +0100

    fixed bugs

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -43,10 +43,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		free(data);
 		return (0);
-		data->value = value_copy;
-		data->next = ht->array[index];
-		ht->array[index] = data;
-
-		return (1);
 	}
+	data->value = value_copy;
+	data->next = ht->array[index];
+	ht->array[index] = data;
+
+	return (1);
 }
